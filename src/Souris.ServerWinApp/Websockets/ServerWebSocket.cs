55109a2af4b54c8f779d6be14b79d5c3ceebd72f
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-17 21:20:38 +0200

    Add project files.

---
+++ src/Souris.ServerWinApp/Websockets/ServerWebSocket.cs
@@ -0,0 +1,25 @@
+﻿using Souris.Server.Websockets.Services;
+using WebSocketSharp.Server;
+
+namespace Souris.Server.Websockets;
+
+class ServerWebSocket
+{
+    private const int PortNumer = 7890;
+    private const string IP_Address = "127.0.0.1";
+
+    private readonly WebSocketServer _webSocketServer;
+
+    public ServerWebSocket()
+    {
+        var serverRoute = $"ws://{IP_Address}:{PortNumer}";
+        _webSocketServer = new WebSocketServer(serverRoute);
+    }
+
+    public void Initialize()
+    {
+        var mouseAutomationEndpoint = "/mouse-automation";
+        _webSocketServer.AddWebSocketService<MouseAutomationWebSockertBehavior>(mouseAutomationEndpoint);
+        _webSocketServer.Start();
+    }
+}
---
+++ src/Souris.ServerWinApp/Websockets/Services/MouseAutomationWebSockertBehavior.cs
@@ -0,0 +1,14 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Souris.Server.Websockets.Services;
+
+class MouseAutomationWebSockertBehavior : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        base.OnMessage(e);
+
+        var data = e.Data;
+    }
+}
