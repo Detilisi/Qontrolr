ece4b87f0c881af0f5fedb507411d71b8180ec05
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-18 23:29:16 +0200

    Update MouseAutomationWebSockertBehavior.cs

--- src/Souris.ServerWinApp/Websockets/Services/MouseAutomationWebSockertBehavior.cs
+++ src/Souris.ServerWinApp/Websockets/Services/MouseAutomationWebSockertBehavior.cs
@@ -1,14 +1,31 @@
-﻿using WebSocketSharp;
+﻿using System.Diagnostics;
+using WebSocketSharp;
 using WebSocketSharp.Server;
+using WindowsInput;
 
 namespace Souris.Server.Websockets.Services;
 
 class MouseAutomationWebSockertBehavior : WebSocketBehavior
 {
+    private readonly InputSimulator _inputSimulator;
+
+    public MouseAutomationWebSockertBehavior()
+    {
+        _inputSimulator = new InputSimulator();
+    }
+
     protected override void OnMessage(MessageEventArgs e)
     {
         base.OnMessage(e);
 
-        var data = e.Data;
+        var parts = e.Data.Split(",");
+
+        if (parts.Length == 2 &&
+            double.TryParse(parts[0], out double deltaX) &&
+            double.TryParse(parts[1], out double deltaY))
+        {
+            Debug.WriteLine((int)deltaX + ", " + (int)deltaY);
+            _inputSimulator.Mouse.MoveMouseBy((int)deltaX, (int)deltaY);
+        }
     }
 }
