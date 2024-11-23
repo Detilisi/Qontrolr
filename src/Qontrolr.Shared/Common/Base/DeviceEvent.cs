a40577fd506643d34f277d37b8254cdb0a2a9888
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-23 16:41:04 +0200

    Add Shared library project

---
+++ src/Qontrolr.Shared/Common/Base/DeviceEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Qontrolr.Shared.Common.Base;
+
+public class DeviceEvent<T>(string name, T data) where T : class
+{
+    public required string Name { get; set; } = name;
+    public required T Data { get; set; } = data;
+}
