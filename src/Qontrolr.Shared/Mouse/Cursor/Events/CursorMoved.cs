59c2678eae37e9de782a726cb02cf0471547796f
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-23 16:54:08 +0200

    Add cursor events

---
+++ src/Qontrolr.Shared/Mouse/Cursor/Events/CursorMoved.cs
@@ -0,0 +1,11 @@
+﻿using Qontrolr.Shared.Common.Base;
+using Qontrolr.Shared.Mouse.Cursor.ValueObjects;
+
+namespace Qontrolr.Shared.Mouse.Cursor.Events;
+
+public class CursorMoved : DeviceEvent<CursorPosition>
+{
+    public CursorMoved(CursorPosition data) : base("cursor.moved", data)
+    {
+    }
+}
