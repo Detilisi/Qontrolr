2fc31ab68a69b6bc65c8fb1710b8dca06556746c
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-25 23:09:16 +0200

    Update MouseAutomation.cs

--- src/Qontrolr.ServerWinApp/Websockets/WebSocketBehaviors/MouseAutomation.cs
+++ src/Qontrolr.ServerWinApp/Websockets/WebSocketBehaviors/MouseAutomation.cs
@@ -3,6 +3,7 @@ using Qontrolr.Shared.Mouse.Button.Events;
 using Qontrolr.Shared.Mouse.Cursor.Events;
 using Qontrolr.Shared.Mouse.Wheel.Enums;
 using Qontrolr.Shared.Mouse.Wheel.Events;
+using System.Diagnostics;
 using System.Text.Json;
 using WebSocketSharp;
 using WebSocketSharp.Server;
@@ -67,7 +68,7 @@ internal class MouseAutomation : WebSocketBehavior
             }
             else if (wheelScrolledEvent.Data == ScrollDirection.Down)
             {
-                _inputSimulator.Mouse.VerticalScroll(-scrollFactor);
+                _inputSimulator.Mouse.VerticalScroll(scrollFactor * -1);
             }
         }
     }    
