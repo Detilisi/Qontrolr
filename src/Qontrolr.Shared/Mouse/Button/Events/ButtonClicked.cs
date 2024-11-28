7635fbb168ab7ec7e8480669512ae7b453745c61
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-11-28 22:44:36 +0200

    Remove old event classes

--- src/Qontrolr.Shared/Mouse/Button/Events/ButtonClicked.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Qontrolr.Shared.Mouse.Button.Events;
-
-public class ButtonClicked : DeviceEvent<ButtonId>
-{
-    public ButtonClicked(ButtonId data) : base("mouse.button.clicked", data)
-    {
-    }
-}
