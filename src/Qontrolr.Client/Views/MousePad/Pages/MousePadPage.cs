500d7738ef38d43d3f75a5c7e1d1ae20fe0bb676
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-05 18:31:13 +0200

    Update MousePadPage.cs

--- src/Qontrolr.Client/Views/MousePad/Pages/MousePadPage.cs
+++ src/Qontrolr.Client/Views/MousePad/Pages/MousePadPage.cs
@@ -1,6 +1,5 @@
 namespace Qontrolr.Client.Views.MousePad.Pages;
 using CommunityToolkit.Maui.Markup;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
 using Qontrolr.Client.Views.Common.Fonts;
 
 public class MousePadPage : ContentPage
@@ -28,14 +27,14 @@ public class MousePadPage : ContentPage
             Children =
             {
                 new Grid(){ BackgroundColor = Colors.Red }.Row(0),
-                new Grid(){ BackgroundColor = Colors.Blue }.Row(1),
-                ControlButton.Row(2),
+                TouchPadControl.Row(1),
+                MouseButtonControl.Row(2),
             }
         };
 	}
 
     //Controls
-    private Grid ControlButton
+    private Grid MouseButtonControl
     {
         get {
             return new Grid()
@@ -53,6 +52,59 @@ public class MousePadPage : ContentPage
                     new Button(){ BackgroundColor = Colors.Gray, CornerRadius = 0 }.Column(0),
                     new Button(){ BackgroundColor = Colors.Gray, CornerRadius = 0 }.Column(1),
                     new Button() { BackgroundColor = Colors.Gray, CornerRadius = 0 }.Column(2),
+                }
+            };
+        }
+    }
+
+    private Grid TouchPadControl
+    {
+        get
+        {
+            return new Grid()
+            {
+                Padding = 0,
+                ColumnSpacing = 1,
+                ColumnDefinitions =
+                [
+                    new ColumnDefinition { Width = new GridLength(9, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                ],
+                Children =
+                {
+                    new Frame()
+                    {
+                        Padding = 0,
+                        CornerRadius = 0,
+                        BackgroundColor = Colors.Blue, 
+                        
+                        Content = new Label()
+                        {
+                            FontSize = 32,
+                            Text = MaterialIconsRound.Swipe,
+                            FontFamily = MaterialIconsRound.FontFamily,
+                            
+                            VerticalOptions = LayoutOptions.Center,
+                            HorizontalOptions = LayoutOptions.Center,
+                        }
+                    }.Column(0),
+                    new Frame()
+                    {
+                        Padding = 0,
+                        CornerRadius = 0,
+                        BackgroundColor = Colors.Yellow,
+
+                        Content = new Label()
+                        {
+                            FontSize = 32,
+                            Text = MaterialIconsRound.Height,
+                            FontFamily = MaterialIconsRound.FontFamily,
+
+                            VerticalOptions = LayoutOptions.Center,
+                            HorizontalOptions = LayoutOptions.Center,
+                        }
+
+                    }.Column(1)
                 }
             };
         }
