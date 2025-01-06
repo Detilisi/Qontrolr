b978130d781b6d43ff424b3b86fb2f03b16c1ae5
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-06 21:45:02 +0200

    Create MaterialIconLabel.cs

---
+++ src/Qontrolr.Client/Views/Common/Controls/MaterialIconLabel.cs
@@ -0,0 +1,16 @@
+﻿using Qontrolr.Client.Views.Common.Fonts;
+
+namespace Qontrolr.Client.Views.Common.Controls;
+
+internal class MaterialIconLabel : Label
+{
+    public MaterialIconLabel(string iconName)
+    {
+        FontSize = 32;
+        Text = iconName;
+        FontFamily = MaterialIconsRound.FontFamily;
+
+        VerticalOptions = LayoutOptions.Center;
+        HorizontalOptions = LayoutOptions.Center;
+    }
+}
