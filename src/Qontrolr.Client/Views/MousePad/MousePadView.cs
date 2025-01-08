76452a044d24b94145f6186b65e46421d69f5bb3
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-08 20:48:06 +0200

    Create MousePadView.cs

---
+++ src/Qontrolr.Client/Views/MousePad/MousePadView.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Maui.Markup;
+using Qontrolr.Client.Views.MousePad.Controls;
+
+namespace Qontrolr.Client.Views.MousePad;
+
+internal class MousePadView : ContentView
+{
+    public MousePadView()
+    {
+        Content = new Grid()
+        {
+            RowSpacing = 1,
+            RowDefinitions =
+            [
+                new RowDefinition { Height = new GridLength(9, GridUnitType.Star) },
+                new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+            ],
+            Children =
+            {
+                new TouchPad(TrackPadPanUpdated, MouseWheelPanUpdated).Row(0),
+                new MouseButtons(MouseButton_Clicked, MouseButton_Pressed, MouseButton_Released).Row(1)
+            }
+        };
+    }
+
+    //MouseButton event Hanlders
+    private void MouseButton_Clicked(Button sender, EventArgs e)
+    {
+
+    }
+
+    private void MouseButton_Pressed(Button sender, EventArgs e)
+    {
+
+    }
+
+    private void MouseButton_Released(Button sender, EventArgs e)
+    {
+
+    }
+
+    //MouseWheel event hanlders
+    private void MouseWheelPanUpdated(Frame sender, PanUpdatedEventArgs e)
+    {
+
+    }
+
+    //MouseWheel event hanlders
+    private void TrackPadPanUpdated(Frame sender, PanUpdatedEventArgs e)
+    {
+
+    }
+}
