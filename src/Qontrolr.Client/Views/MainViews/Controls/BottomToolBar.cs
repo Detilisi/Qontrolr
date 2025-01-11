9a17165334200a4b2d4244e36cbf7710bf6bf026
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-12 00:10:10 +0200

    Update BottomToolBar.cs

--- src/Qontrolr.Client/Views/MainViews/Controls/BottomToolBar.cs
+++ src/Qontrolr.Client/Views/MainViews/Controls/BottomToolBar.cs
@@ -1,31 +1,31 @@
 ﻿using CommunityToolkit.Maui.Markup;
 using Qontrolr.Client.Views.Common.Controls;
+using Qontrolr.Client.Views.Common.Controls.Base;
 using Qontrolr.Client.Views.Common.Fonts;
 
 namespace Qontrolr.Client.Views.MainViews.Controls;
 
-internal class BottomToolBar : Grid 
+internal class BottomToolBar : CustomGridControl
 {
     public BottomToolBar(Action<MaterialIconButton, EventArgs> buttonsClicked)
     {
-        // Initialize Grid properties
-        InitializeGridDefinitions();
+        InitializeGrid();
 
         // Create and add buttons
-        var mouseButton = CreateButton("mouse", MaterialIconsRound.Mouse, buttonsClicked);
-        var keyboardButton = CreateButton("keyboard", MaterialIconsRound.Keyboard_alt, buttonsClicked);
-        var gamepadButton = CreateButton("gamepad", MaterialIconsRound.Sports_esports, buttonsClicked);
-        var mediapadButton = CreateButton("media", MaterialIconsRound.Play_circle, buttonsClicked);
+        var mouseButton = CreateIconButton("mouse", MaterialIconsRound.Mouse, clicked: buttonsClicked);
+        var keyboardButton = CreateIconButton("keyboard", MaterialIconsRound.Keyboard_alt, clicked: buttonsClicked);
+        var gamepadButton = CreateIconButton("gamepad", MaterialIconsRound.Sports_esports, clicked: buttonsClicked);
+        var mediapadButton = CreateIconButton("media", MaterialIconsRound.Play_circle, clicked: buttonsClicked);
 
         // Add buttons to Grid
-        AddButtonToGrid(mouseButton, column: 0);
-        AddButtonToGrid(keyboardButton, column: 1);
-        AddButtonToGrid(gamepadButton, column: 2);
-        AddButtonToGrid(mediapadButton, column: 3);
+        AddToGrid(mouseButton, column: 0);
+        AddToGrid(keyboardButton, column: 1);
+        AddToGrid(gamepadButton, column: 2);
+        AddToGrid(mediapadButton, column: 3);
 
     }
 
-    private void InitializeGridDefinitions()
+    protected override void InitializeGrid()
     {
         Padding = 10;
         ColumnSpacing = 10;
@@ -34,27 +34,5 @@ internal class BottomToolBar : Grid
         ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
         ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
         ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-    }
-
-    private void AddButtonToGrid(MaterialIconButton button, int column = 0, int row = 0)
-    {
-        button.Column(column).Row(row);
-        Children.Add(button);
-    }
-
-    private MaterialIconButton CreateButton
-    (
-        string buttonId,
-        string buttonIcon,
-        Action<MaterialIconButton, EventArgs> buttonClicked
-    )
-    {
-        var button = new MaterialIconButton(buttonIcon)
-        {
-            ClassId = buttonId
-        };
-
-        button.Clicked += (s, e) => buttonClicked(button, e);
-        return button;
     }
 }
