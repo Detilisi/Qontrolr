6bc4b446d1713854750adb1d4ccfd213c110cb7b
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-11 23:09:38 +0200

    Update MainPage.cs

--- src/Qontrolr.Client/Views/MainPage.cs
+++ src/Qontrolr.Client/Views/MainPage.cs
@@ -2,15 +2,18 @@ using CommunityToolkit.Maui.Markup;
 using Qontrolr.Client.ViewModels.MousePad;
 using Qontrolr.Client.Views.Common.Controls;
 using Qontrolr.Client.Views.Common.Fonts;
+using Qontrolr.Client.Views.MediaPad;
 using Qontrolr.Client.Views.MousePad;
 
 namespace Qontrolr.Client.Views;
 
 public class MainPage : ContentPage
 {
+    //Contruction
 	public MainPage()
 	{
         //Set up Main view
+        var bottomToolBar = CreateBottomToolBar();
         CurrentView = new MousePadView(new MousePadViewModel());
 
         Content = new Grid
@@ -24,36 +27,85 @@ public class MainPage : ContentPage
             Children =
             {
                 CurrentView.Row(0),
-                BottomToolBar.Row(1)
+                bottomToolBar.Row(1)
             }
         };
     }
 
     //View elements
-    private ContentView CurrentView { get; set; }
-    private Grid BottomToolBar
+    private ContentView? _currentView;
+    private ContentView CurrentView
     {
-        get
+        get => _currentView ?? new();
+        set
         {
-            return new Grid()
+            if (_currentView == value) return;
+
+            _currentView = value;
+            OnPropertyChanged(nameof(CurrentView));
+            (Content as Grid)?.Children.RemoveAt(0); // Remove old view
+            (Content as Grid)?.Children.Insert(0, _currentView); // Add new view
+        }
+    }
+
+    //Helper methods
+    private Grid CreateBottomToolBar()
+    {
+        return new Grid()
+        {
+            Padding = 10,
+            ColumnSpacing = 10,
+            ColumnDefinitions =
+            [
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+            ],
+            Children =
             {
-                Padding = 10,
-                ColumnSpacing = 10,
-                ColumnDefinitions =
-                [
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                ],
-                Children =
-                {
-                    new MaterialIconButton(MaterialIconsRound.Mouse).Column(0),
-                    new MaterialIconButton(MaterialIconsRound.Keyboard_alt).Column(1),
-                    new MaterialIconButton(MaterialIconsRound.Sports_esports).Column(2),
-                    new MaterialIconButton(MaterialIconsRound.Play_circle).Column(3),
-                }
-            };
+                CreateButton("mouse", MaterialIconsRound.Mouse).Column(0),
+                CreateButton("keyboard", MaterialIconsRound.Keyboard_alt).Column(1),
+                CreateButton("gamepad", MaterialIconsRound.Sports_esports).Column(2),
+                CreateButton("media", MaterialIconsRound.Play_circle).Column(3),
+            }
+        };
+    }
+
+    private MaterialIconButton CreateButton
+    (
+        string buttonId,
+        string buttonIcon
+    )
+    {
+        var newButton = new MaterialIconButton(buttonIcon)
+        {
+            ClassId = buttonId
+        };
+
+        newButton.Clicked += NewButton_Clicked;
+        return newButton;
+    }
+
+    //Event handlers
+    private void NewButton_Clicked(object? sender, EventArgs e)
+    {
+        if (sender is not Button button) return;
+
+        switch (button.ClassId)
+        {
+            case "mouse":
+                CurrentView = new MousePadView(new MousePadViewModel());
+                break;
+            case "keyboard":
+                break;
+            case "gamepad":
+                break;
+            case "media":
+                CurrentView = new MediaPadView();
+                break;
+            default:
+                break;
         }
     }
 }
