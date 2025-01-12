feddef209d3fb4274a5fc19ad39e5e033fab3669
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-12 16:56:07 +0200

    Create KeyPadView.cs

---
+++ src/Qontrolr.Client/Views/SubViews/KeyPad/KeyPadView.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui.Markup;
+using Qontrolr.Client.Views.SubViews.KeyPad.Controls;
+
+namespace Qontrolr.Client.Views.SubViews.KeyPad;
+
+internal class KeyPadView : ContentView
+{
+	public KeyPadView()
+	{
+        var entryView = new Entry();
+        entryView.Focus();
+
+        Content = new Grid()
+        {
+            RowSpacing = 1,
+            RowDefinitions =
+            [
+                new RowDefinition { Height = new GridLength(6, GridUnitType.Star) },
+                new RowDefinition { Height = new GridLength(4, GridUnitType.Star) },
+            ],
+            Children =
+            {
+                new WindowsKeyButtons(Keypad_Clicked).Row(0),
+                entryView.Row(1)
+            }
+        };
+    }
+
+    private void Keypad_Clicked(Button sender, EventArgs e)
+    {
+    }
+}
