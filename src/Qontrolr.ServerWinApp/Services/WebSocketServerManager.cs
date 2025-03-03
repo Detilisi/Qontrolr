09865a99cbbf0c44eb5ae1d1a66e03dbe7348388
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-03-03 22:06:55 +0200

    Create WebSocketServerManager.cs

---
+++ src/Qontrolr.ServerWinApp/Services/WebSocketServerManager.cs
@@ -0,0 +1,60 @@
+﻿using Qontrolr.Server.Websockets.WebSocketBehaviors;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using WebSocketSharp.Server;
+
+namespace Qontrolr.Server.Services;
+
+public class WebSocketServerManager
+{
+    // Construct
+    private readonly WebSocketServer _webSocketServer;
+    public WebSocketServerManager()
+    {
+        SeverUrl = GenerateQontrolrUrl(7890);
+        if (string.IsNullOrWhiteSpace(SeverUrl))
+        {
+            throw new InvalidOperationException("Server URL is not initialized.");
+        }
+
+        _webSocketServer = new WebSocketServer(SeverUrl);
+        _webSocketServer.AddWebSocketService<MouseAutomation>(MouseAutomation.Endpoint);
+    }
+
+    public string SeverUrl { get; set; }
+
+    // Public Methods
+    public void Start()
+    {
+        if (_webSocketServer.IsListening) return;
+
+        _webSocketServer.Start();
+    }
+
+    public void Stop()
+    {
+        if (!_webSocketServer.IsListening) return;
+
+        _webSocketServer.Stop();
+    }
+
+    //Helpers
+    private static string GenerateQontrolrUrl(int port)
+    {
+        try
+        {
+            var addressList = Dns.GetHostEntry(Dns.GetHostName())?.AddressList;
+            if (addressList == null || addressList.Length == 0) return string.Empty;
+
+            var ipv4Address = addressList?.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+
+            return ipv4Address != null ? $"ws://{ipv4Address}:{port}" : string.Empty;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error initializing BaseUrl: {ex.Message}");
+            return string.Empty;
+        }
+    }
+}
