6b220879038ed261be1e60b35852f317c02be8dd
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-03-11 22:05:42 +0200

    Create PopupService.cs

---
+++ src/Qontrolr.Client/Services/PopupService.cs
@@ -0,0 +1,20 @@
+﻿namespace Qontrolr.Client.Services;
+
+public static class PopupService
+{
+    public static async Task ShowAlertAsync(string title, string message)
+    {
+        var page = Application.Current?.MainPage;
+        if (page == null) return;
+
+        await page.DisplayAlert(title, message, "OK");
+    }
+
+    public static async Task<object?> ShowPopupAsync(Popup popup)
+    {
+        var page = Application.Current?.MainPage;
+        if (page == null) return null;
+
+        return await page.ShowPopupAsync(popup);
+    }
+}
